78098429a7a118ceaae840ec9e2c16b1106b6064
rishadroshanpt <rishadroshan27778@gmail.com>
2024-12-30 15:36:19 +0530

    new react projrct

---
+++ react-components/src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import {Link,Outlet} from 'react-router-dom'
+
+function Navbar(){
+    return(
+        <>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="javascript:void(0)">Logo</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="mynavbar">
+                <ul className="navbar-nav me-auto">
+                    <li className="nav-item">
+                        <Link to='/' className="nav-link"> Home </Link>
+                    {/* <a className="nav-link" href="javascript:void(0)">Home</a> */}
+                    </li>
+                    <li className="nav-item">
+                    <Link to='About' className="nav-link"> About </Link>
+                    {/* <a className="nav-link" href="javascript:void(0)">About</a> */}
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+        <Outlet/>
+        </>
+    )
+}
+export default Navbar
